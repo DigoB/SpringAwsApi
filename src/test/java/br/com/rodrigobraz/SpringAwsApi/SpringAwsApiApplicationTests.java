6ec86946c98423c31fa2f4dfab060b00362e1f02
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-03-16 01:20:24 -0300

    Project created

---
+++ src/test/java/br/com/rodrigobraz/SpringAwsApi/SpringAwsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.SpringAwsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
