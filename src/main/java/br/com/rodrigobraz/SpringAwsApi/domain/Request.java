1f2d1b2628c4087fd759f0c671fa1911e15187a5
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-03-16 02:04:21 -0300

    feat: Entities mapped

--- src/main/java/br/com/rodrigobraz/SpringAwsApi/domain/Request.java
+++ src/main/java/br/com/rodrigobraz/SpringAwsApi/domain/Request.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,13 +13,31 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Request {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String subject;
+
+    @Column(length = 75, nullable = false, columnDefinition = "text")
     private String description;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIME)
     private Date timestamp;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "request")
     private List<RequestStage> statuses = new ArrayList<>();
 }
--- src/main/java/br/com/rodrigobraz/SpringAwsApi/domain/RequestStage.java
+++ src/main/java/br/com/rodrigobraz/SpringAwsApi/domain/RequestStage.java
@@ -1,20 +1,39 @@
 package br.com.rodrigobraz.SpringAwsApi.domain;
 
+import br.com.rodrigobraz.SpringAwsApi.domain.enums.RequestStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class RequestStage {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIME)
     private Date date;
-    private RequestStage stage;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RequestStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
